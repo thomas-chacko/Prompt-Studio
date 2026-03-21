ef972d7e289821f04294e451e51aa4eccfa30d87
thomas-chacko <thomaschacko180@gmail.com>
2026-03-21 17:34:55 +0530

    prisma verision changed to 6

--- server/prisma.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-    seed: 'node prisma/seed.js',
-  },
-})
